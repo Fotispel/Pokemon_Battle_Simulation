edb6a4e496fcf8ebfd8eead2eaeafbaf7a699151
trlef19 <82327147+trlef19@users.noreply.github.com>
2023-12-26 14:44:43 +0200

    new Pokemon class

--- pokemon_actions.h
+++ pokemon_actions.h
@@ -1,1 +1,29 @@
-#include <stdio.h>
+#include "poke_important.h"
+
+class Pokemon{
+	private:
+		string name;
+		string type;
+		int health_points;
+	public:
+		Pokemon(string name,string type,int hp){
+			this->name=name;
+			this->type=type;
+			this->health_points=hp;
+		}
+		string getPokemonName(){
+			return this->name;
+		}
+		string getPokemonType(){
+			return this->type;
+		}
+		int getHealthPoints(){
+			return this->health_points;
+		}
+		void setHealthPoints(int hp){
+			this->health_points=hp;
+		}
+		void PrintPokemon(){
+			cout<<"Name: "<<this->name<<"Type: "<<this->type<<"Health points: "<<this->health_points<<endl;
+		}
+};
